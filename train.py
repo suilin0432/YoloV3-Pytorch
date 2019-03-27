d186517aeb31074ffca79a160f9d89baeb5f9900
隋霖 <401569798@qq.com>
2019-03-27 16:31:39 +0800

    begin to build the train.py

--- train.py
+++ train.py
@@ -1,3 +1,14 @@
-from config.train_cfg import TRAIN_PARAMS
+from config.train_cfg import TRAIN_PARAMS as cfg
+import torch
+import torch.nn as nn
+import numpy
+from model.YoloNet import YoloNet
+from model.YoloLoss import YoloLoss
+from torch.utils.data import DataLoader
+from util.PictureDataLoader import PictureDataLoader
+import os
+import logging
+import cv2
 
-cfg = TRAIN_PARAMS
+
+CUDA = torch.cuda.is_available() and cfg["use_cuda"]
